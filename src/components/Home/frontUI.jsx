41a2a6a2ee8d9cbf4c11d4dc17f1bb3eeca64fb1
arslanali2641 <arslanali2641@gmail.com>
2019-10-17 20:50:55 +0500

    my first-day work

--- src/components/Home/frontUI.jsx
+++ src/components/Home/frontUI.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
 import "./../../Stylesheets/Home/frontUI.css";
+import map from "../../images/map.png";
 class FrontUi extends Component {
   state = {};
   render() {
-    return <div>Front UI</div>;
+    return (
+      <div id="main-div">
+        <div className="main-div">
+          <div className="top-div">
+            <div className="map-div">
+              <img src={map} alt="map" />
+            </div>
+            <div className="pink-divs"></div>
+            <div className="pink1"></div>
+            <div className="pink2"></div>
+            <div className="pink3"></div>
+            <div className="pink4">
+              <h1>Your Prescription.</h1>
+              <h1>Your terms.</h1>
+              <p>
+                Name your price and let pharmacies bid to earn your business on
+                our transparent blockchain platform
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
